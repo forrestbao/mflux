5b49205fae657781763ce59fba1db4ad3e355daf
Wu Jiang <wu@morediff.info>
2015-01-13 12:03:39 -0500

    New branch for cross validation and SVC
    - SVC does not work at all
    - KNN seems to be having very low score or accuracy
    
    ```
    (sci)wjiang@ark:~/workspace/bitbucket/influx% python get_model.py
    v       model   score/accuracy
    1       KNeighborsRegressor     0.00373724667623 (+/- 0.114101327295)
    2       KNeighborsRegressor     -0.147970235425 (+/- 0.424421195795)
    3       KNeighborsRegressor     -0.123769066461 (+/- 0.31298162848)
    4       KNeighborsRegressor     -0.109093413933 (+/- 0.28271509007)
    5       KNeighborsRegressor     -0.0490658133817 (+/- 0.312333240669)
    6       KNeighborsRegressor     -0.0787047515645 (+/- 0.345543037627)
    7       KNeighborsRegressor     -0.117116324411 (+/- 0.139522040691)
    8       KNeighborsRegressor     -0.143436487067 (+/- 0.445356623254)
    9       KNeighborsRegressor     -0.376785929865 (+/- 0.701155090677)
    10      KNeighborsRegressor     -0.134858012348 (+/- 0.676597440388)
    11      KNeighborsRegressor     -0.00188894824536 (+/- 0.406555184488)
    12      KNeighborsRegressor     0.00523607986853 (+/- 0.340312866206)
    13      KNeighborsRegressor     -0.0373591182505 (+/- 0.197140029454)
    14      KNeighborsRegressor     0.018189642675 (+/- 0.286351281563)
    15      KNeighborsRegressor     -0.117559222168 (+/- 0.429949115587)
    16      KNeighborsRegressor     0.017572024638 (+/- 0.289545637513)
    17      KNeighborsRegressor     0.00470691490755 (+/- 0.271357828137)
    18      KNeighborsRegressor     0.00457822157588 (+/- 0.257838918074)
    19      KNeighborsRegressor     -0.0201184535157 (+/- 0.239319332015)
    20      KNeighborsRegressor     -0.0471272373539 (+/- 0.269409911649)
    21      KNeighborsRegressor     -0.012301301525 (+/- 0.476741303831)
    22      KNeighborsRegressor     0.00358753119531 (+/- 0.386020944985)
    23      KNeighborsRegressor     0.0174429482284 (+/- 0.170790158337)
    24      KNeighborsRegressor     -0.358634584138 (+/- 0.378455215474)
    25      KNeighborsRegressor     0.115196093301 (+/- 0.411422053679)
    26      KNeighborsRegressor     -3.22631744582 (+/- 8.92129148868)
    27      KNeighborsRegressor     -1.38803755777 (+/- 2.53016577915)
    28      KNeighborsRegressor     -0.199868397531 (+/- 0.666701088195)
    29      KNeighborsRegressor     -0.754768669645 (+/- 1.56946698293)
    ```

--- get_model.py
+++ get_model.py
@@ -1,5 +1,30 @@
 # extract the training data from spreadsheet
 
+import traceback, sys, code
+import cPickle
+
+import numpy
+from sklearn import cross_validation, preprocessing
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVC
+
+
+class RegressionModel(object):
+    """A help class to store model's name and the actual model.
+    """
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.model = eval(name)(**kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+KNN_MODEL = RegressionModel("KNeighborsRegressor", n_neighbors=5,
+                            weights="distance")
+SVC_MODEL = RegressionModel("SVC", kernel="linear", C=1)
+
+
 def read_spreadsheet(File):
     """Turn spreadsheet into matrixes for training
 
@@ -9,15 +34,15 @@ def read_spreadsheet(File):
 	Training_data: a dict, keys are EMPs (e.g., v1, v2, etc.),
                    values are 2-tuples (Feature, Label), where
                    Feature is a 2-D list, each sublist is 24-D feature vector for one sample
-                   and 
+                   and
                    Label is a 1-D list, labels for all samples.
-	
+
 
     Notes
     ============
     EMPs are N.A. for some samples, training features were dropped for them.
     That's why we need one training feature matrix for each EMP.
-    
+
     We have 29 influxes values to predict and thus the index/key for Training_data goes from 1 to 29
 
     AA is 26 for 0-index
@@ -47,7 +72,7 @@ def read_spreadsheet(File):
             try:
                 Vector = map(float, Vector)
             except ValueError:
-                print Vector    
+                print Vector
 
             # Now create the dictionaries we need, one dictionary for each influx
             for i in range(1, 29+1):
@@ -56,7 +81,7 @@ def read_spreadsheet(File):
                     Label = float(Label)
                 except ValueError:
 #                    print Label, "=>"
-#                    print Line 
+#                    print Line
                     continue # this label for this influx is not numeric
 
                 Training_data[i][0].append(Vector) # add a row to feature vectors
@@ -64,18 +89,17 @@ def read_spreadsheet(File):
 
     return Training_data
 
+
 def one_hot_encode_features(Training_data):
     """Use one-hot encoder to represent categorical features
 
-    Feature from 1 to 7 are categorical features: 
+    Feature from 1 to 7 are categorical features:
     Species, reactor, nutrient, oxygen, engineering method, MFA and extra energy
 
     """
-    import sklearn.preprocessing
-    import numpy
     Encoded_training_data, Encoders = {}, {}
     for vID, (Vectors, Targets) in Training_data.iteritems():
-            Encoder = sklearn.preprocessing.OneHotEncoder()
+            Encoder = preprocessing.OneHotEncoder()
             Vectors = numpy.array(Vectors) # 2-D array
             Encoded_Categorical_Features = Encoder.fit_transform(Vectors[:, 0:6+1])
             Encoded_Categorical_Features = Encoded_Categorical_Features.toarray()
@@ -84,6 +108,7 @@ def one_hot_encode_features(Training_data):
             Encoders[vID] = Encoder
     return Encoded_training_data, Encoders
 
+
 def standardize_features(Training_data):
     """Standarize feature vectors for each influx
 
@@ -91,7 +116,6 @@ def standardize_features(Training_data):
     Scalers[i].transform(X)
 
     """
-    from sklearn import preprocessing
     Std_training_data, Scalers = {}, {}
     for vID, (Vectors, Labels) in Training_data.iteritems():
         Vectors_scaled = preprocessing.scale(Vectors)
@@ -101,30 +125,50 @@ def standardize_features(Training_data):
 
     return Std_training_data, Scalers
 
+
 def train_model(Training_data):
     """Train a regression model for each of the 29 influxes
 
-    Returns 
+    Returns
     ================
     Models: dict, keys are influx indexes and values are regression models
 
     """
-    from sklearn.neighbors import KNeighborsRegressor
     Models = {}
     for i in range(1, 29+1):
         (Vectors, Label) = Training_data[i]
-        Model = KNeighborsRegressor(n_neighbors=5, weights='distance') # initialize the model
+        Model = KNN_MODEL.model
         Model.fit(Vectors, Label) # train the model
         Models[i] = Model
+        return Models
 
-    return Models
+
+def cross_validation_model(training_data, model):
+    print("v\tmodel\tscore/accuracy")
+    for i in range(1, 29 + 1):
+        vectors, label = training_data[i]
+        # FIXME: always ran into Value error (nan)
+        # x_train, x_test, y_train, y_test = cross_validation.train_test_split(
+        #     vectors, label, test_size=0.3, random_state=0)
+        # clf = model.model.fit(x_train, y_train)
+        # score = clf.score(x_test, y_test)
+        # print("{}\t{}\t{}".format(i, model, score))
+
+        scores = cross_validation.cross_val_score(model.model, vectors, label)
+        print("{}\t{}\t{} (+/- {})"
+              .format(i, model, scores.mean(), scores.std() * 2))
+
 
 if __name__ == "__main__":
     Training_data = read_spreadsheet("1111_forrest.csv")
     Encoded_training_data, Encoders = one_hot_encode_features(Training_data)
     Std_training_data, Scalers = standardize_features(Encoded_training_data)
+
+    cross_validation_model(Std_training_data, KNN_MODEL)
+    # FIXME: ValueError: Can't handle mix of continuous and multiclass
+    cross_validation_model(Std_training_data, SVC_MODEL)
+
     Models = train_model(Std_training_data)
-    import cPickle
     cPickle.dump(Models, open("models_knn.p", "wb"))
     cPickle.dump(Scalers, open("scalers.p", "wb"))
     cPickle.dump(Encoders, open("encoders.p", "wb"))
